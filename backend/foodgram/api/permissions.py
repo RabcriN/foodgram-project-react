50489e71aecb82ae21d565948dd884fd08fc9c1c
RabcriN <rabcrin@gmail.com>
2023-02-04 04:04:03 +0300

    IsAuthorAdminOrReadOnly pep8

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -5,6 +5,6 @@ class IsAuthorAdminOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-            or obj.author == request.user or request.user.is_admin 
+            or obj.author == request.user or request.user.is_admin
             or request.user.is_superuser
         )
